34001f873079dc812d9eab4cedebb9cd3cae236c
EasterMaster <teo.mastropasqua@gmail.com>
2018-12-03 18:39:27 +0100

    Created IPowrUp and the powerUp RocketAccelerator

---
+++ Assets/Scripts/PowerUp/PowerUpBase.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public abstract class PowerUpBase : MonoBehaviour , IPowerUp
+{
+    public abstract void Effect(IPlayer _player);
+
+    /// <summary>
+    /// Chiamato quando un Player entra nell'area di trigger
+    /// </summary>
+    /// <param name="_player"></param>
+    public virtual void OnTake()
+    {
+        
+    }
+
+    protected virtual void OnTriggerEnter(Collider other)
+    {
+        IPlayer player = other.GetComponent<IPlayer>();
+        if (player != null)
+        {
+            OnTake();
+            Effect(player);
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/PowerUp/RocketAccelerator.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RocketAccelerator : PowerUpBase
+{
+    [SerializeField]
+    float addedSpeed;
+
+    public override void Effect(IPlayer _player)
+    {
+        Rocket rocket;
+        if (_player.ID == 2)
+        {
+            rocket = FindObjectOfType<Rocket>();
+            rocket.MovementSpeed += addedSpeed;
+            Debug.Log(rocket.MovementSpeed);
+        }
+    }
+}
