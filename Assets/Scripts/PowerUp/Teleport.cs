21111bfff5c73b976a5a7ef462be7e7e4e047cd2
Emanuele Papagno <emanuelelele92@gmail.com>
2018-12-14 03:05:05 +0100

    reinserito script restart, aggiunto script Teleport, aggiunti in scena rocket boosters e teleports

---
+++ Assets/Scripts/PowerUp/Teleport.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleport : MonoBehaviour {
+
+
+    [SerializeField]
+    Transform tp1destination;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        other.gameObject.transform.position = tp1destination.transform.position;
+        gameObject.SetActive(false);
+    }
+}
