2baa5c342e16e6d9f6c98be5009d843edeb62a61
Emanuele Papagno <emanuelelele92@gmail.com>
2018-12-19 19:40:13 +0100

    aggiunta collisione tra player, e prima divisione arena

---
+++ Assets/Scripts/Arena/WallMovLeft.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallMovLeft : MonoBehaviour
+{
+
+
+    public GameObject wall;
+
+    public Vector3 StartingPosition;
+    public Vector3 firstTransition;
+
+    public float distance = 2f;
+
+    public float lerpTime = 2;
+
+
+    public float currentLerpTime;
+
+    private void Start()
+    {
+        StartingPosition = wall.transform.position;
+        firstTransition = wall.transform.position + Vector3.left * distance;
+    }
+
+    private void Update()
+    {
+        currentLerpTime += Time.deltaTime;
+        if (currentLerpTime >= lerpTime)
+        {
+            currentLerpTime = lerpTime;
+        }
+
+        float perc = currentLerpTime / lerpTime;
+
+        wall.transform.position = Vector3.Lerp(StartingPosition, firstTransition, perc);
+    }
+
+
+}
