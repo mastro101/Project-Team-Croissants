421d28894f1419304c88e0528657115313e6fbd7
Emanuele Papagno <emanuelelele92@gmail.com>
2018-12-21 01:07:05 +0100

    modificata scena programmer, aggiunto script movUP

---
+++ Assets/Scripts/Arena/WallMovUp.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallMovUp : MonoBehaviour
+{
+
+
+    public GameObject wall;
+
+    public Vector3 StartPos;
+    public Vector3 EndPos;
+
+    public float distancebetween = 2f;
+
+    public float lerptimer = 2;
+
+
+    public float currentLerpTimer;
+
+    private void Start()
+    {
+        StartPos = wall.transform.position;
+        EndPos = wall.transform.position + new Vector3(0, 0, 1) * distancebetween;
+    }
+
+    private void Update()
+    {
+        currentLerpTimer += Time.deltaTime;
+        if (currentLerpTimer >= lerptimer)
+        {
+            currentLerpTimer = lerptimer;
+        }
+
+        float perc = currentLerpTimer / lerptimer;
+
+        wall.transform.position = Vector3.Lerp(StartPos, EndPos, perc);
+    }
+
+
+}
