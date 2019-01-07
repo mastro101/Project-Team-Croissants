215ae199ceada6ebe0f93e9efc8b409a186cba78
EasterMaster <teo.mastropasqua@gmail.com>
2019-01-07 16:02:14 +0100

    Modifica programmer scene con telecamera centrata

--- Assets/Scripts/Item/Slowing.cs
+++ Assets/Scripts/Item/Slowing.cs
@@ -3,8 +3,19 @@ using System.Collections;
 
 public class Slowing : ItemBase
 {
+    float OriginalSpeed;
+
     public override void Effect(IPlayer _player)
     {
-        
+        _player.MovementSpeed -= (_player.MovementSpeed / 100) * 20;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        IPlayer player = other.GetComponent<IPlayer>();
+        if (player != null)
+        {
+
+        }
     }
 }
