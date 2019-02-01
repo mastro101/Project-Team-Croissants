51c451fad4e2ff49ce7d28f12b33b014fffacbec
andreaancona <a.ancona.bb@gmail.com>
2019-02-01 16:37:20 +0100

    Create SelectPlayer Scene

---
+++ Assets/Scripts/SelectPlayer.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class SelectPlayer : MonoBehaviour
+{
+    [SerializeField]
+    string sceneName;
+    [SerializeField]
+    TextMeshProUGUI Player1Text, Player2Text;
+    bool onePressed, twoPressed;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Joystick1Button0))
+        {
+            onePressed = true;
+            Player1Text.text = "Ok";
+            check();
+        }
+
+        if (Input.GetKeyDown(KeyCode.Joystick2Button0))
+        {
+            twoPressed = true;
+            Player2Text.text = "Ok";
+            check();
+        }
+    }
+
+    void check()
+    {
+        if (onePressed && twoPressed)
+        {
+            SceneManager.LoadScene(sceneName);
+        }
+    }
+}
