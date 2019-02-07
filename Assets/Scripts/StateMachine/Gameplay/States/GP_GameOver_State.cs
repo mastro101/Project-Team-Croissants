cfa41158e455eef140df276c64bce56f5626cf56
EasterMaster <teo.mastropasqua@gmail.com>
2019-02-07 23:08:25 +0100

    ProBuilder+ fix light + FineGioco

---
+++ Assets/Scripts/StateMachine/Gameplay/States/GP_GameOver_State.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace StateMachine.Gameplay
+{
+    public class GP_GameOver_State : GP_Base_State
+    {
+        protected override string stateName
+        {
+            get
+            {
+                return "GameOver";
+            }
+        }
+
+        public override void Enter()
+        {
+            base.Enter();
+            context.EndRoundPanel.SetActive(true);
+
+        }
+
+        public override void Tick()
+        {
+            base.Tick();
+            if (Input.GetKeyDown(KeyCode.JoystickButton0))
+            {
+                context.BaseExitState();
+            }
+        }
+    }
+}
