a5a4a4c7f88e8e15564ec00ba28b2287803e4ba0
andreaancona <a.ancona.bb@gmail.com>
2019-02-22 13:27:45 +0100

    Creato MapGenerator

---
+++ Assets/Editor/MapEditor.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(MapGenerator))]
+public class MapEditor : Editor
+{
+
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+
+        MapGenerator map = target as MapGenerator;
+
+        if (GUILayout.Button("GenerateMap"))
+        {
+            // Date la colpa a Papagno
+            if (EditorUtility.DisplayDialog("CREARE NUOVA ARENA?", "PERDERAI TUTTI I DATI NON SALVATI SEI DAVVERO SICURO SICURO?",
+                "YOLO", "ora mi sembra una cazzata"))
+            {
+                map.GenerateMap();
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/MapGenerator.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapGenerator : MonoBehaviour
+{
+    public Transform TilePrefab;
+    public Vector2 MapSize;
+
+    /// <summary>
+    /// Distanza tra le Tile
+    /// </summary>
+    public float Outline;
+
+    public void GenerateMap()
+    {
+        string holderName = "GeneratedMap";
+        /// Distrugge La griglia precedente 
+        if (transform.Find(holderName))
+        {
+            DestroyImmediate(transform.Find(holderName).gameObject);
+        }
+
+        /// Crea Un GO padre dove contenere le tile
+        Transform mapHolder = new GameObject(holderName).transform;
+        mapHolder.parent = transform;
+
+        for (int x = 0; x < MapSize.x; x++)
+        {
+            for (int y = 0; y < MapSize.y; y++)
+            {
+                Vector3 tilePosition = new Vector3(-MapSize.x / 2 + 0.5f + (Outline * x), transform.position.y, -MapSize.y / 2 + 0.5f + (Outline * y));
+                Transform newTile = Instantiate(TilePrefab, tilePosition, Quaternion.Euler(Vector3.zero));
+                newTile.parent = mapHolder;
+            }
+        }
+    }
+}
