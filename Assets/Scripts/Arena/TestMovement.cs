8d35e4714b37206f6c050d82f9b499bef2bdc8d5
Marco Marchitelli <marchitelli97@libero.it>
2019-03-01 18:23:05 +0100

    aggiunti png rosso\blu da mettere sotto il player

---
+++ Assets/Scripts/Arena/TestMovement.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+using StateMachine.Gameplay;
+using System;
+
+public class TestMovement : MonoBehaviour
+{
+    Transform wallToMove;
+
+    GameplaySM gameplaysm;
+
+
+    [SerializeField]
+    Vector3 newPosition;
+
+    [SerializeField] float movementDuration;
+
+    [SerializeField] float WaitTime;
+
+
+    Tween tween;
+
+
+    private void Awake()
+    {
+        wallToMove = transform;
+    }
+
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            MoveArena();
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            spawn();
+        }
+    }
+
+
+    void MoveArena()
+    {
+        tween = wallToMove.DOMove(newPosition, movementDuration).SetDelay<Tween>(WaitTime).SetEase(Ease.Linear).SetAutoKill<Tween>(false).SetRelative();
+    }
+
+
+    public void spawn()
+    {
+        tween.Rewind(true);
+    }
+}
+
---
+++ Assets/Scripts/Player/SpritePlayerStretch.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class SpritePlayerStretch : MonoBehaviour
+{
+
+    public GameObject highlightedPlayer;
+
+    Tween tween;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        //highlightedPlayer.transform.DOScale(0.4f, 3).
+
+        Gira();
+
+
+    }
+
+
+    void Gira()
+    {
+        //tween.SetLoops<Tween>(-1, LoopType.Yoyo);
+        tween = highlightedPlayer.transform.DOScale(0.3f, 1).SetLoops<Tween>(-1, LoopType.Yoyo);
+    }
+
+}
