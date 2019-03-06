be51eb6e90b2a25dba840cafe17b015839232d5f
Emanuele Papagno <emanuelelele92@gmail.com>
2019-03-06 18:36:24 +0100

    Aggiunto 20 bottoni alla SceneSelector, aggiunto mirino png nei player

---
+++ Assets/Scripts/UI/ButtonSceneSelector.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonSceneSelector : MonoBehaviour
+{
+
+    public int numeroScena;
+
+    public void OnClickCallBack()
+    {
+        SceneManager.LoadScene(numeroScena);
+    }
+
+    public void SelectScene()
+    {
+        SceneManager.LoadScene(numeroScena);
+    }
+
+}
