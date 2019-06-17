39f484c4026a8c10f1e2ec8dd5d5c17b5dbe5d1f
EasterMaster <teo.mastropasqua@gmail.com>
2019-06-17 20:33:40 +0200

    TimerVFX

---
+++ Assets/Scripts/TimerVFX.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimerVFX : MonoBehaviour
+{
+    [SerializeField] ParticleSystem VFX;
+    [SerializeField] float timerToPlay;
+    [Space]
+    [SerializeField] bool neverStop = true;
+    [SerializeField] float timerToStop;
+
+    private void Awake()
+    {
+        VFX.Stop();
+    }
+
+    private void Start()
+    {
+        StartCoroutine(StartVFX());
+    }
+
+    IEnumerator StartVFX()
+    {
+        yield return new WaitForSeconds(timerToPlay);
+        VFX.Play();
+        if (!neverStop)
+            StartCoroutine(StopVFX());
+    }
+
+    IEnumerator StopVFX()
+    {
+        yield return new WaitForSeconds(timerToStop);
+        VFX.Stop();
+    }
+
+}
