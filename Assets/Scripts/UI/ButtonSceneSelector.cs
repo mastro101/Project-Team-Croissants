d0490fbaf6a0e4fb9db0497d9aa6cbabcba7f0ad
emanuelepapagno <emanuelelele92@gmail.com>
2019-06-21 01:37:13 +0200

    test loading screen su bottone scena denittis horizontal

--- Assets/Scripts/UI/ButtonSceneSelector.cs
+++ Assets/Scripts/UI/ButtonSceneSelector.cs
@@ -2,11 +2,16 @@
 using UnityEngine.SceneManagement;
 using StateMachine.Menu;
 using System.Collections;
+using UnityEngine.UI;
+using TMPro;
 
 public class ButtonSceneSelector : MonoBehaviour
 {
     public string NomeScena;
-    public GameObject loadTest;
+    public GameObject loadTestImageScreen;
+    public Slider loadingBar;
+    public TextMeshProUGUI loadText;
+
 
     AsyncOperation async;
 
@@ -25,19 +30,24 @@ public class ButtonSceneSelector : MonoBehaviour
     
     IEnumerator caricalivello()
     {
-        loadTest.SetActive(true);
+        loadTestImageScreen.SetActive(true);
         
         FindObjectOfType<GameManager>().SelectedLevel = NomeScena;
         FindObjectOfType<MenuSM>().goNext();
         async = SceneManager.LoadSceneAsync("Ambientazione2");
 
+       
+
         async.allowSceneActivation = false;
 
         while(async.isDone == false)
         {
-            Debug.Log(async.progress);
+            float progress = Mathf.Clamp01(async.progress / 0.9f);
+            loadingBar.value = progress;
+            loadText.text = "Loading...         " + (progress * 100f).ToString() + "%";
+            Debug.Log(progress);
             if (async.progress == 0.9f)
-            {   
+            {
                 async.allowSceneActivation = true;
             }
             yield return null;
