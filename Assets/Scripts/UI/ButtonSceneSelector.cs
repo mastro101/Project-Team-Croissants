dc1b92ad1643046723d2f56329b2d6cef737bab9
emanuelepapagno <emanuelelele92@gmail.com>
2019-06-21 01:04:56 +0200

    fixato anfora scena denittis, aggiunta loadingScene(non implementata), modificato ButtonSceneSelector script, test loading screen

--- Assets/Scripts/UI/ButtonSceneSelector.cs
+++ Assets/Scripts/UI/ButtonSceneSelector.cs
@@ -1,10 +1,14 @@
 ﻿using UnityEngine;
 using UnityEngine.SceneManagement;
 using StateMachine.Menu;
+using System.Collections;
 
 public class ButtonSceneSelector : MonoBehaviour
 {
     public string NomeScena;
+    public GameObject loadTest;
+
+    AsyncOperation async;
 
     public void SelectScene()
     {
@@ -12,4 +16,33 @@ public class ButtonSceneSelector : MonoBehaviour
         FindObjectOfType<MenuSM>().goNext();
         SceneManager.LoadScene("Ambientazione2");
     }
+
+    public void LoadScreen()
+    {
+        StartCoroutine(caricalivello());
+
+    }
+    
+    IEnumerator caricalivello()
+    {
+        loadTest.SetActive(true);
+        
+        FindObjectOfType<GameManager>().SelectedLevel = NomeScena;
+        FindObjectOfType<MenuSM>().goNext();
+        async = SceneManager.LoadSceneAsync("Ambientazione2");
+
+        async.allowSceneActivation = false;
+
+        while(async.isDone == false)
+        {
+            Debug.Log(async.progress);
+            if (async.progress == 0.9f)
+            {   
+                async.allowSceneActivation = true;
+            }
+            yield return null;
+        }
+
+    }
+   
 }
