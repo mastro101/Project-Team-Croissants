87020e6de75e44d088eb8c9ca374cc199096d348
EasterMaster <teo.mastropasqua@gmail.com>
2019-07-16 17:02:57 +0200

    nuove schermate di caricamento + edit grandezza testo scene selector

--- Assets/Scripts/SelectLevel/SelectLevelMenu.cs
+++ Assets/Scripts/SelectLevel/SelectLevelMenu.cs
@@ -18,10 +18,13 @@ public class SelectLevelMenu : MonoBehaviour
 
     public GameObject loadTestImageScreen;
     AsyncOperation async;
-    public Slider loadingBar;
+    public Image loadingBar;
     public TextMeshProUGUI loadText;
     [SerializeField]
+    TextMeshProUGUI loadingText;
+    [SerializeField]
     Sprite[] loadingBackgrounds;
+    string[,] loadingString;
 
     bool choosed;
 
@@ -37,6 +40,7 @@ public class SelectLevelMenu : MonoBehaviour
         choosed = false;
         selectLevel.changeLevel = changeIcon;
         changeIcon();
+        setText();
     }
 
     void changeIcon()
@@ -109,7 +113,11 @@ public class SelectLevelMenu : MonoBehaviour
 
     IEnumerator caricalivello()
     {
-        loadTestImageScreen.GetComponent<Image>().sprite = loadingBackgrounds[Random.Range(0, loadingBackgrounds.Length)];
+        int randomCharacterInt = Random.Range(0, loadingBackgrounds.Length);
+        int randomTextInt = Random.Range(0, 2);
+
+        loadTestImageScreen.GetComponent<Image>().sprite = loadingBackgrounds[randomCharacterInt];
+        loadingText.text = loadingString[randomCharacterInt, randomTextInt];
         loadTestImageScreen.SetActive(true);
 
         gameManager.SelectedLevel = selectLevel.currentLevel.ID;
@@ -123,7 +131,7 @@ public class SelectLevelMenu : MonoBehaviour
         while (async.isDone == false)
         {
             float progress = Mathf.Clamp01(async.progress / 0.9f);
-            loadingBar.value = progress;
+            loadingBar.fillAmount = progress;
             loadText.text = "Loading...         " + (progress * 99f).ToString("F0") + "%";
             Debug.Log(progress);
             if (async.progress == 0.9f)
@@ -135,4 +143,14 @@ public class SelectLevelMenu : MonoBehaviour
 
     }
 
+    void setText()
+    {
+        loadingString = new string[,]
+        {
+            { "Trinity has a real knack for programming. And also explosions!" , "I heard Trinity whispering something about the PartyX having me..." } ,
+            { "Veeky’s favourite motto is: “Punk ain’t dead… you are!”" , "Watch your step, if you don’t want to blow up." } ,
+            { "Watch out, SameID has friends on the other side!" , "When I got close to SameID, my mind got hazy..." } ,
+            { "Did you hear that hellish sound? I think it was Wendigo’s laughter!" , "Ouch, something cut me! ...oh, it was Wendigo’s edge." }
+        };
+    }
 }
